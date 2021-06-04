3cdac74211a046b6b6c1b693a878d3514802cb7c
noman2002 <nomn2002@gmail.com>
2021-06-04 21:56:12 +0530

    xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -4,45 +4,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
-    final player = AudioCache();
-    player.play("assets/note$soundNumber.wav");
-  }
-
-  Expanded buildKey(Color color, int soundNumber) {
-    return Expanded(
-      child: TextButton(
-        child: Text(''),
-        style: TextButton.styleFrom(
-          backgroundColor: color,
-        ),
-        onPressed: () {
-          playSound(soundNumber);
-        },
-      ),
-    );
-  }
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
+  Widget build(BuildContext context) => MaterialApp(
+        home: XylophonePage(),
+      );
+}
+
+class XylophonePage extends StatelessWidget {
+  final player = AudioCache();
+  @override
+  Widget build(BuildContext context) => Scaffold(
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(Colors.red, 1),
-              buildKey(Colors.orange, 2),
-              buildKey(Colors.yellow, 3),
-              buildKey(Colors.green, 4),
-              buildKey(Colors.teal, 5),
-              buildKey(Colors.blue, 6),
-              buildKey(Colors.purple, 7),
-            ],
-          ),
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildKey(color: Colors.red, keyNumber: 1),
+                buildKey(color: Colors.orange, keyNumber: 2),
+                buildKey(color: Colors.yellow, keyNumber: 3),
+                buildKey(color: Colors.green, keyNumber: 4),
+                buildKey(color: Colors.blue.shade300, keyNumber: 5),
+                buildKey(color: Colors.blue.shade700, keyNumber: 6),
+                buildKey(color: Colors.purple, keyNumber: 7),
+              ]),
         ),
-      ),
-    );
-  }
+      );
+
+  Widget buildKey({Color? color, int? keyNumber}) => Expanded(
+        child: MaterialButton(
+          color: color,
+          onPressed: () {
+            player.play("note$keyNumber.wav");
+          },
+        ),
+      );
 }
+
